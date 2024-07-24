3ebdbca6b8f61d14c3e3e0e4d2629e5249cc4e2d
Colin <colin.albs@gmail.com>
2024-07-24 11:23:39 -0500

    Tested HW2. It works. Next: Write README documentation.

--- src/HW.cpp
+++ src/HW.cpp
@@ -77,7 +77,9 @@ class HullWhite: private General {
         }
 };
 
-// Mathematically equivalent to G2++: https://www.math.kth.se/matstat/seminarier/reports/M-exjobb12/120220b.pdf  proof starts at botton of page 19
+// This implementation is technically G2++.
+// The Hull-White Two-Factor model is mathematically equivalent to G2++: 
+// https://www.math.kth.se/matstat/seminarier/reports/M-exjobb12/120220b.pdf proof starts at botton of page 19
 class HullWhiteTwoFactor: private General {
     private:
         double r_0, kappa_1, kappa_2, rho; // rho = instantaneous correlation of two-dimensional Brownian motion
@@ -92,11 +94,11 @@ class HullWhiteTwoFactor: private General {
             r_0 = r_0_con;
             theta = theta_con;
 
-            kappa_1 = kappa_con[0];
+            kappa_1 = kappas_con[0];
             kappa_2 = kappas_con[1];
 
-            sigma_1 = sigma_con[0];
-            sigma_2 = sigma_con[1];
+            sigma_1 = sigmas_con[0];
+            sigma_2 = sigmas_con[1];
 
             rho = rho_con;
         }
@@ -104,30 +106,28 @@ class HullWhiteTwoFactor: private General {
 
         std::vector<double> simulated_value(const int& num_time_steps, const double& T) {
             check_same_length({theta, sigma_1, sigma_2});
+            assert(-1 <= rho && rho <= 1);
 
             auto d_t = T / num_time_steps;
 
-            std::vector<double> rates(num_time_steps, 0), xates(num_time_steps, 0);
+            std::vector<double> rates(num_time_steps, 0);
+            double x_rates = 0, y_rates = 0;
             rates[0] = r_0;
-
-            double W_1 = 0, W_2 = 0;
-
+            
             for (int i = 0; i < num_time_steps; ++i) {
                 int parameter_index = get_parameter_index(i, num_time_steps, theta.size());
-                auto Z_1 = dist(gen), Z_2 = dist(gen);
-
-                W_1 += sqrt(d_t) * Z_1;
-                W_2 += sqrt(d_t) * (rho * Z_1 + sqrt(1 - pow(rho, 2)) * Z_2);
+                auto W_1 = sqrt(d_t) * dist(gen), W_2 = rho * W_1 + sqrt(1 - pow(rho, 2)) * sqrt(d_t) * dist(gen);
 
                 auto theta_curr = theta[parameter_index], sigma_1_curr = sigma_1[parameter_index], sigma_2_curr = sigma_2[parameter_index];
 
-                rates[i] = rates[i - 1] + (theta_curr + xates[i - 1] - kappa_1 * rates[i - 1]) + sigma_1_curr * W_1;
-                xates[i] = xates[i - 1] - kappa_2 * xates[i - 1] * d_t + sigma_2_curr * W_2;
+                x_rates += -kappa_1 * x_rates * d_t + sigma_1_curr * sqrt(d_t) * W_1;
+                y_rates += -kappa_2 * y_rates * d_t + sigma_2_curr * sqrt(d_t) * W_2;
+
+                rates[i] = std::max(r_0 + x_rates + y_rates, 0.0);
             }
 
             return rates;
         }
-
 };
 
 
@@ -137,10 +137,9 @@ int main() {
     // std::cout << "Hull-White simulated rate: " << hw_temp[hw_temp.size() - 1] << std::endl;
 
 
-// ADD OTHER DYNAMICS
-    // HullWhiteTwoFactor hw2_testing_class(0.05, {0.03, 0.02, 0.025}, 0.2, {0.01, 0.015, 0.02});
-    // std::vector<double> hw2_temp = hw2_testing_class.simulated_value(10000, 1);
-    // std::cout << "Two-Factor Hull-White simulated rate: " << hw2_temp[hw2_temp.size() - 1] << std::endl;
+    HullWhiteTwoFactor hw2_testing_class(0.08, {0.03, 0.02, 0.025}, {0.2, 0.99}, {{0.65, 0.6, 0.7}, {0.01, 0.015, 0.02}}, 0.5);
+    std::vector<double> hw2_temp = hw2_testing_class.simulated_value(10000, 1);
+    std::cout << "Two-Factor Hull-White simulated rate: " << hw2_temp[hw2_temp.size() - 1] << std::endl;
 
     return 0;
 }
